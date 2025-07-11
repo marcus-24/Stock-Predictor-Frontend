26925afd89b0e3a3ce1bd6eb5b76f7517db95e09
Marcus C. Allen, Ph.D. <marcusCallen24@gmail.com>
2025-07-11 18:55:25 -0400

    Added StockElements React Component

--- src/components/StockStats.tsx
+++ src/components/StockStats.tsx
@@ -3,38 +3,67 @@ import { IStock } from "../interfaces";
 
 const roundPrice = (price: number) => Math.round(price * 100) / 100;
 
-function getLatestPrice(prices: IStock[]): string {
-  const lastPrice = prices[prices.length - 1].Close;
-  return roundPrice(lastPrice).toLocaleString("en-US"); // round to closest hundredth
+const priceChange = (prices: IStock[]) =>
+  prices[prices.length - 1].Close - prices[0].Close;
+
+const getLatestPrice = (prices: IStock[]) =>
+  roundPrice(prices[prices.length - 1].Close);
+
+function percentChange(priceChange: number, firstPrice: number): number {
+  const percentChange = priceChange / firstPrice;
+  return Math.round(percentChange * 10000) / 100;
 }
 
-function percentChangeString(prices: IStock[]): string {
-  const percentChange =
-    (prices[prices.length - 1].Close - prices[0].Close) / prices[0].Close;
-  const roundPercentChange = Math.round(percentChange * 10000) / 100;
-  const displayString = roundPercentChange > 0 ? "↑" : "↓";
-  return displayString + `${roundPercentChange}%`;
-}
-
-function priceChangeString(prices: IStock[]): string {
-  const priceChange = prices[prices.length - 1].Close - prices[0].Close;
+function priceChangeString(priceChange: number): string {
   const displayString = priceChange > 0 ? "+" : "";
   return displayString + `${roundPrice(priceChange)}`;
 }
 
-function generateStyle(prices: IStock[]): { [key: string]: string } {
-  const priceChange = prices[prices.length - 1].Close - prices[0].Close;
+function generateArrowStyle(priceChange: number): { [key: string]: string } {
   if (priceChange > 0) {
     return {
       color: "darkgreen",
       backgroundColor: "rgba(85, 255, 0, 0.2)",
     };
-  } else {
-    return {
-      color: "darkred",
-      backgroundColor: "rgba(255, 0, 0, 0.2)",
-    };
   }
+  return {
+    color: "darkred",
+    backgroundColor: "rgba(255, 0, 0, 0.2)",
+  };
+}
+
+function generateStatText(priceChange: number): { [key: string]: string } {
+  if (priceChange > 0) {
+    return { color: "green" };
+  }
+
+  return { color: "red" };
+}
+
+function StatsElement({
+  latestPrice,
+  priceChange,
+  percentChange,
+  title,
+}: {
+  latestPrice: number;
+  priceChange: number;
+  percentChange: number;
+  title: string;
+}): ReactElement {
+  return (
+    <div className="stock-stats">
+      <div className="arrow" style={generateArrowStyle(priceChange)}>
+        {percentChange > 0 ? "↑" : "↓"}
+      </div>
+      <div>{title}</div>
+      <div>{latestPrice}</div>
+      <div style={generateStatText(priceChange)}>{`${percentChange}%`}</div>
+      <div style={generateStatText(priceChange)}>
+        {priceChangeString(priceChange)}
+      </div>
+    </div>
+  );
 }
 
 function StockStats({
@@ -45,21 +74,29 @@ function StockStats({
   preds: IStock[];
 }): ReactElement {
   const latestStock = useMemo(() => getLatestPrice(stocks), [stocks]);
-  const stockPercentChange = percentChangeString(stocks);
-  const stockPriceChange = priceChangeString(stocks);
+  const stockChange = useMemo(() => priceChange(stocks), [stocks]);
+  const stockPercentChange = percentChange(stockChange, stocks[0].Close);
+  // const stockPriceChangeStr = priceChangeString(stockChange);
 
   const latestPred = useMemo(() => getLatestPrice(preds), [preds]);
-  const predPercentChange = percentChangeString(preds);
-  const predPriceChange = priceChangeString(preds);
+  const predChange = useMemo(() => priceChange(preds), [preds]);
+  const predPercentChange = percentChange(predChange, preds[0].Close);
+  // const predPriceChangeStr = priceChangeString(predChange);
 
   return (
-    <div className="stock-stats">
-      <div style={generateStyle(stocks)}>
-        {latestStock} {stockPercentChange} {stockPriceChange}
-      </div>
-      <div style={generateStyle(preds)}>
-        {latestPred} {predPercentChange} {predPriceChange}
-      </div>
+    <div className="hud-container">
+      <StatsElement
+        latestPrice={latestStock}
+        priceChange={stockChange}
+        percentChange={stockPercentChange}
+        title="Current Stock Statistics"
+      />
+      <StatsElement
+        latestPrice={latestPred}
+        priceChange={predChange}
+        percentChange={predPercentChange}
+        title="Predicted Stock Statistics"
+      />
     </div>
   );
 }
